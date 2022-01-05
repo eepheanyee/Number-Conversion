fadd1260e4b243cd11f26391882fd83b2ff88881
kenechukwu ifediorah <45775913+eepheanyee@users.noreply.github.com>
2022-01-05 03:10:04 +0100

    Added a GUI using the Tkinter library
    I decided to add a GUI using the tkinter library and also caught some bugs that could break the code. This is a better version and I'll still work on refactoring it

--- number_conversion.py
+++ number_conversion.py
@@ -1,3 +1,8 @@
+# Adding GUI using the Tkinter module
+import tkinter as tk
+from types import NoneType
+
+
 # Python program to print a given number in words
 
 # string at index 0 is not used. This is to make list indexing easier 
@@ -51,18 +56,66 @@ def num_to_words(num):
             return(convert(num//1000)+"thousand")
         return(convert(num//1000)+"thousand, "+convert(num%1000))
     if 6 < l <= 9:
+        if num%1000000 == 0:
+            return(convert(num//1000000)+"million")
+        if (num%1000000)%1000 == 0:
+            return(convert(num//1000000)+"million, "+convert((num%1000000)//1000)+"thousand")
+        if (num%1000000)%1000 == 0:
+                return(convert(num//1000000)+"million, "+convert((num%1000)%1000))
         return(convert(num//1000000)+"million, "+convert((num%1000000)//1000)+"thousand, "+convert((num%1000)%1000))
     if 9 < l <= 12:
+        if num%1000000000 == 0:
+            return(convert(num//1000000000)+"billion")
+        if (num%1000000000)%1000000 == 0:
+            return(convert(num//1000000000)+"billion, "+convert((num%1000000000)//1000000)+"million")
+        if ((num%1000000000)%1000000)%1000 == 0:
+            return(convert(num//1000000000)+"billion, "+convert((num%1000000000)//1000000)+"million, "+convert(((num%1000000000)%1000000)//1000)+"thousand")
         return(convert(num//1000000000)+"billion, "+convert((num%1000000000)//1000000)+"million, "+convert(((num%1000000000)%1000000)//1000)+"thousand, "+convert((num%1000)%1000))
     if 12 < l <= 15:
+        if num%1000000000000 == 0:
+            return(convert(num//1000000000000)+"trillion")
+        if (num%1000000000000)%1000000000 == 0:
+            return(convert(num//1000000000000)+"trillion, "+convert((num%1000000000000)//1000000000)+"billion")
+        if ((num%1000000000000)%1000000000)%1000000 == 0:
+            return(convert(num//1000000000000)+"trillion, "+convert((num%1000000000000)//1000000000)+"billion, "+convert(((num%1000000000000)%1000000000)//1000000)+"million")
+        if (((num%1000000000000)%1000000000)%1000000)%1000 == 0:
+            return(convert(num//1000000000000)+"trillion, "+convert((num%1000000000000)//1000000000)+"billion, "+convert(((num%1000000000000)%1000000000)//1000000)+"million, "+convert(((num%1000000000)%1000000)//1000)+"thousand")
         return(convert(num//1000000000000)+"trillion, "+convert((num%1000000000000)//1000000000)+"billion, "+convert((num%1000000000)//1000000)+"million, "+convert(((num%1000000000)%1000000)//1000)+"thousand, "+convert((num%1000)%1000))
-    
-ans = "Y"
-while ans.lower() == "y":
+
+root = tk.Tk()   # Opens the GUI window
+root.title = ("Number converter")
+
+
+instructions = tk.Label(root, text="Python program to print a given number in words. Enter the number you want to convert", font="Raleway")
+instructions.grid(columnspan=3, column=0, row=0)
+
+my_input = tk.Entry(root, width=50, borderwidth=5)
+my_input.grid(row=1, column=0, columnspan=3, padx=10, pady=10)
+
+def return_entry():
+    """Gets and prints the content of the entry"""
     try:
-        num = int(input("Input a number to convert: "))
-        print(num_to_words(num))
-        ans = input("Type 'Y' or 'y' to continue and any other key to quit: ")
-    except ValueError:
-        print("Please enter a correct number!!!")
-        ans = input("Type 'Y' or 'y' to continue and any other key to quit: ")
+        content = int(my_input.get())
+        return content
+    except:
+        text_box = tk.Text(root, height=5, width=50, padx=15, pady=15)
+        text_box.insert(1.0, "Please enter a real number")
+        text_box.grid(column=1, row=3)
+
+def printing():
+    try:
+        x = return_entry()
+        y = num_to_words(x)
+        text_box = tk.Text(root, height=5, width=50, padx=15, pady=15)
+        text_box.insert(1.0, y)
+        text_box.grid(column=1, row=3)
+        print(y)
+    except TypeError:
+        text_box = tk.Text(root, height=5, width=50, padx=15, pady=15)
+        text_box.insert(1.0, "Please enter a real number")
+        text_box.grid(column=1, row=3)
+
+convert_button = tk.Button(root, text="Convert", padx=30, pady=15, command=printing)
+convert_button.grid(row=2, column=1, )
+
+root.mainloop()     # Closes the GUI window
